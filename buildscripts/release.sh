c41cf9a1c3838a9ffc0c65b6ec276fec77479010
Steffen Bleul <sbl@blacklabelops.com>
2018-03-02 22:12:00 +0100

    #34 Moving command to script.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -5,6 +5,7 @@
 #------------------
 export BITBUCKET_VERSION=5.8.1
 
+docker build -t blacklabelops/bitbucket .
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker push blacklabelops/bitbucket
 docker tag blacklabelops/bitbucket:${BITBUCKET_VERSION} blacklabelops/bitbucket:${BITBUCKET_VERSION}
