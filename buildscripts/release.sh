7dba996ad173586ff02ff1e1fcaabdcb2c89fe41
Steffen Bleul <sbl@blacklabelops.com>
2018-03-02 22:15:04 +0100

    #34 Tagging untagged to tagged.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -8,5 +8,5 @@ export BITBUCKET_VERSION=5.8.1
 docker build -t blacklabelops/bitbucket .
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker push blacklabelops/bitbucket
-docker tag blacklabelops/bitbucket:${BITBUCKET_VERSION} blacklabelops/bitbucket:${BITBUCKET_VERSION}
+docker tag blacklabelops/bitbucket blacklabelops/bitbucket:${BITBUCKET_VERSION}
 docker push blacklabelops/bitbucket:${BITBUCKET_VERSION}
