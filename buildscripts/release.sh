3832daeb0e5af11cee09672f0cd9b28d74122efa
Steffen Bleul <sbl@blacklabelops.com>
2018-03-02 22:06:04 +0100

    #34 Second try setting release tag

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -4,3 +4,8 @@
 # CONTAINER VARIABLES
 #------------------
 export BITBUCKET_VERSION=5.8.1
+
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+docker push blacklabelops/bitbucket
+docker tag blacklabelops/bitbucket:${BITBUCKET_VERSION} blacklabelops/bitbucket:${BITBUCKET_VERSION}
+docker push blacklabelops/bitbucket:${BITBUCKET_VERSION}
