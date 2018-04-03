9fb705a0c56eb807a126d4919ed7ee10108a4ae1
Steffen Bleul <sbl@blacklabelops.com>
2018-04-03 21:27:46 +0200

    Fix: Repository Push only for master branch.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -6,7 +6,10 @@
 export BITBUCKET_VERSION=5.9.1
 
 docker build -t blacklabelops/bitbucket .
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-docker push blacklabelops/bitbucket
-docker tag blacklabelops/bitbucket blacklabelops/bitbucket:${BITBUCKET_VERSION}
-docker push blacklabelops/bitbucket:${BITBUCKET_VERSION}
+
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+  docker push blacklabelops/bitbucket
+  docker tag blacklabelops/bitbucket blacklabelops/bitbucket:${BITBUCKET_VERSION}
+  docker push blacklabelops/bitbucket:${BITBUCKET_VERSION}
+fi
